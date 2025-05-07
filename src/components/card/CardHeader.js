2613ef71292bc7e9ea2528de74b108fdc20e8c76
Pepijn Koets <pepkoets@hotmail.com>
2025-05-07 16:52:47 +0200

    mobil5

--- src/components/card/CardHeader.js
+++ src/components/card/CardHeader.js
@@ -31,7 +31,7 @@ export default function CardHeader({ profile, user, isPublicView = false }) {
   };
 
   return (
-    <div> {/* Wrapper div */}
+    <div className="px-2 sm:px-4 ..."> {/* Wrapper div */}
       {/* Header Afbeelding */}
       <div
         className={styles.headerImage}
@@ -50,7 +50,7 @@ export default function CardHeader({ profile, user, isPublicView = false }) {
             <Image
               src={profile.avatar_url}
               alt={profile?.name || user?.email || 'Profile Avatar'}
-              width={1000} // Exacte grootte specificeren
+              width={80} // Exacte grootte specificeren
               height={80} // Exacte grootte specificeren
               className={styles.avatarImage} // Gebruik class voor object-fit etc.
             />
